426938180b1ba3c23918df81782a3c854f7417b8
TGorgijoska <gorgijoska.t@hotmail.com>
2021-10-02 00:11:09 +0200

    printBook()

--- scripts/script.js
+++ scripts/script.js
@@ -7,10 +7,29 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const read = document.querySelector('#read');
+const cards = document.querySelector('.book-cards');
 const removeBtn = document.querySelectorAll('.removeBtn');
 const readBtn = document.querySelectorAll('.readBtn');
 let library = [];
-let newbook;
+let newbook, lenght;
+// ---- for creating a book-card
+let div = document.createElement('div');
+div.className = "card";
+let h2 = document.createElement('h2');
+h2.className = "title";
+let pa = document.createElement('p');
+pa.className = "author";
+pa.textContent = "by ";
+let pp = document.createElement('p');
+pp.className = "pages";
+pp.innerHTML = "pages: ";
+let btn1 = document.createElement('button');
+btn1.classList.add("btn");
+btn1.classList.add("readBtn");
+let btn2 = document.createElement('button');
+btn2.classList.add("btn");
+btn2.classList.add("removeBtn");
+btn2.textContent = "X";
 
 class Book {
     constructor(title, author, numPages, read) {
@@ -41,22 +60,36 @@ removeBtn.forEach(btn => btn.addEventListener('click', () => deleteBook(btn.data
 readBtn.forEach(btn => btn.addEventListener('click', () => changeReadStatus(btn)));
 
 function addBookToLibrary(book) {
-    library.push(book);
+    return library.push(book);
 }
 
 function createBook() {
     // event.preventDefault();
     if(checkInput()){
         newbook = new Book(title.value, author.value, pages.value, read.value);
-        addBookToLibrary(newbook);
+        lenght = addBookToLibrary(newbook);
         // formReset();
+        printBook(lenght);
         closeModalForm();
     }       
 }
-// todo: printBooks(); changeReadStatus()
+// todo: 
 
-function printBooks(params) {
-    
+function printBook(cardnum) {
+    div.dataset.cardnum = cardnum;
+    h2.textContent = title.value;
+    pa.textContent = "by " + author.value;
+    pp.textContent = pages.value + " pages";
+    btn1.dataset.cardnum = cardnum;
+    btn1.textContent = read.value;
+    btn2.dataset.cardnum = cardnum;
+
+    div.appendChild(h2);
+    div.appendChild(pa);
+    div.appendChild(pp);
+    div.appendChild(btn1);
+    div.appendChild(btn2);
+    cards.appendChild(div);
 }
 function changeReadStatus(btn) {
     if(btn.textContent == "read"){
